2259174810e99188cf8057fb63d79ec7f0766de6
pandasandeggs <theteatreehouse@gmail.com>
2019-12-18 16:49:42 -0600

    added background and js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-js 
+///////////////// INDEX PAGE /////////////////////
+
+// Buttons
+let aboutButton = document.createElement("button");
+aboutButton.innerHTML = "About";
+let portfolioButton = document.createElement("button");
+portfolioButton.innerHTML = "Portfolio";
+let contactButton = document.createElement("button");
+contactButton.innerHTML = "Contact";
+
+// Append buttons
+var about = document.getElementById("menu");
+about.appendChild(aboutButton);
+var portfolio = document.getElementById("menu");
+portfolio.appendChild(portfolioButton);
+var contact = document.getElementById("menu");
+contact.appendChild(contactButton);
+
+// Button Event Handlers
+aboutButton.addEventListener ("click", function() {
+    alert("Clicked about!");
+});
+portfolioButton.addEventListener ("click", function() {
+    alert("Clicked portfolio!");
+});
+contactButton.addEventListener ("click", function() {
+    alert("Clicked contact!");
+});
